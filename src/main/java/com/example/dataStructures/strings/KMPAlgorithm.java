3fdf035d98c61b681854b21d057355d9ac5e4223
Ganta-Abhigna <abhigna.ganta@gmail.com>
2024-01-21 18:33:50 +0530

    refactr

--- src/main/java/com/example/dataStructures/strings/KMPAlgorithm.java
+++ src/main/java/com/example/dataStructures/strings/KMPAlgorithm.java
@@ -1,5 +1,10 @@
 package com.example.dataStructures.strings;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class KMPAlgorithm {
     public static void main(String[] args){
         String st = "ABABDABACDABABCABAB";
@@ -62,4 +67,32 @@ public class KMPAlgorithm {
     }
 
 
+    public static class Anagrams {
+        public static void main(String[] args){
+            String st[] = {"ct","abhi", "tac","god","dog","act","test"};
+            printAnagramUsingHashMap(st);
+        }
+        public static void printAnagramUsingHashMap(String[] st){
+            HashMap<String, List<String>> map=new HashMap<>();
+            for(int i=0;i<st.length;i++){
+                String word = st[i];
+                char[] letters=word.toCharArray();
+                Arrays.sort(letters);
+                String newWord = new String(letters);
+                if(map.containsKey(newWord)){
+                    map.get(newWord).add(word);
+                }
+                else{
+                    List<String> list = new ArrayList<>();
+                    list.add(word);
+                    map.put(newWord,list);
+                }
+            }
+            for (List<String> list: map.values()) {
+                if(list.size()>1)
+                System.out.println(list);
+            }
+        }
+
+    }
 }
